4669b211cabc671586ead0e5080d5eb92d8b524d
ff6347 <hash@ff6347.email>
2022-08-16 11:12:11 +0200

    chore: latest state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { UserProvider } from "@supabase/supabase-auth-helpers/react";
+import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<main className={"dark"}>
+			<UserProvider supabaseClient={supabaseClient}>
+				<Component {...pageProps} />
+			</UserProvider>
+		</main>
+	);
 }
-
-export default MyApp
